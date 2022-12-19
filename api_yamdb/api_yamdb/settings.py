1c173fd8ba94741f08b16a7ab43fbc7a131da3fd
Serdron <96815264+Serdron@users.noreply.github.com>
2022-12-19 15:08:08 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,13 +3,11 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', 
-    default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
+SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default = False)
 
 ALLOWED_HOSTS = ['*']
-
 
 
 INSTALLED_APPS = [
